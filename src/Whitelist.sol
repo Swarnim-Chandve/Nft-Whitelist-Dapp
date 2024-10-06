d88c0f923d50d175179ec066da34794826143b15
Swarnim <swarnimchandve23@gmail.com>
2024-10-07 01:19:17 +0530

    second commit

--- src/Whitelist.sol
+++ src/Whitelist.sol
@@ -1,27 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-
 contract Whitelist {
     uint8 public maxWhitelistedAddresses;
 
-
     mapping(address => bool) public whitelistedAddresses;
-
 
     uint8 public numAddressesWhitelisted;
 
-
-    constructor (uint8 _maxWhitelistedAddresses) {
+    constructor(uint8 _maxWhitelistedAddresses) {
         maxWhitelistedAddresses = _maxWhitelistedAddresses;
     }
-
-
 
     function addAddressToWhitelist() public {
         require(!whitelistedAddresses[msg.sender], "Sender is already been whitelisted");
 
-        require(numAddressesWhitelisted < maxWhitelistedAddresses,"More addresses cant be added, limit reached");
+        require(numAddressesWhitelisted < maxWhitelistedAddresses, "More addresses cant be added, limit reached");
 
         whitelistedAddresses[msg.sender] = true;
 
